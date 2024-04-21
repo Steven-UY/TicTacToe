6227fba8180e7281cc58cbff6fc4431748ae0d41
Steven Uy <Steven@supersimple.dev>
2024-04-21 19:41:15 -0400

    revamped static frontend

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./public/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-yellow' : '#ffffe3',
+      }
+    },
   },
   plugins: [],
 }
