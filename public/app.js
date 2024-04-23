2bff6167d6ae3a98542e17e6edd6ac66dfa2c0b5
Steven Uy <steven.uy10@gmail.com>
2024-04-23 00:40:41 -0400

    added functionality for x to appear when cell is clicked

---
+++ public/app.js
@@ -0,0 +1,15 @@
+
+const cells = document.querySelectorAll('#board button');
+//selects all button elements that are children of ID board
+
+cells.forEach(cell => { //iterates over each cell in cells, cells is defined above   
+    cell.addEventListener('click', function() {//adds an event listener to current button 'cell'
+        const span = cell.querySelector('span');
+        if (span.textContent === '') {
+            span.textContent = 'X';  // Add 'X' to the clicked cell
+            span.classList.add('text-custom-yellow'); // Optionally add a class for styling
+        }
+    });
+});
+
+
