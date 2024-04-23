8c31f9e24fc7d2d50185c657f85cfca9dd4423e2
Steven Uy <steven.uy10@gmail.com>
2024-04-23 00:51:13 -0400

    fixed the button and span configuration

--- public/app.js
+++ public/app.js
@@ -2,7 +2,7 @@
 const cells = document.querySelectorAll('#board button');
 //selects all button elements that are children of ID board
 
-cells.forEach(cell => { //iterates over each cell in cells, cells is defined above   
+cells.forEach(function(cell) { //iterates over each cell in cells, cells is defined above   
     cell.addEventListener('click', function() {//adds an event listener to current button 'cell'
         const span = cell.querySelector('span');
         if (span.textContent === '') {
